fd7b143f156884104b8841aadf963771ad512419
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-20 21:51:50 +0545

    modified

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -7,3 +7,12 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
