462d93f2b5a24b7a57673fc61840687af0657db7
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-20 21:55:27 +0545

    modified

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -7,6 +7,11 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # Move the ship to the right.
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
