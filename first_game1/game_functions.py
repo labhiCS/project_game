84735e299aaac9b98e68db73025b46f006d04f54
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-20 22:11:55 +0545

    modified

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -2,7 +2,7 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
