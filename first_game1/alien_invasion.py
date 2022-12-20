ac08db368c573ce4319a76d963efd4cb01be10ca
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-20 22:03:41 +0545

    modified

--- first_game1/alien_invasion.py
+++ first_game1/alien_invasion.py
@@ -24,6 +24,7 @@ def run_game():
     #Start the main loop for the game.
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
         #Watch for keyboard and mouse events.
