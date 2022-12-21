791c97f18b9af5382152e0912f08e63413abff5f
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-21 21:05:27 +0545

    Creating Bullet

--- first_game1/alien_invasion.py
+++ first_game1/alien_invasion.py
@@ -1,12 +1,12 @@
 import sys
-
 import pygame
 
 from settings import Settings
-
 from ship import Ship
+from pygame.sprite import Group
 
 import game_functions as gf
+
 
 def run_game():
     # Initialize pygame, settings, and screen object
@@ -21,12 +21,16 @@ def run_game():
 
     # Make a a ship
     ship = Ship(ai_settings, screen)
+
+    #make a group to store bullet in.
+    bullets= Group()
     
     #Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
         #Watch for keyboard and mouse events.
         for event in pygame.event.get():
