b72f9dd3bc429b4e12f2e0bc974556a3e375617d
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-22 22:24:13 +0545

    modified

--- first_game1/alien_invasion.py
+++ first_game1/alien_invasion.py
@@ -31,17 +31,5 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_screen(ai_settings, screen, ship, bullets)
-
-        #Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-                
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
         
 run_game()
