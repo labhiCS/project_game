da4c2340c01263c9eb6ef20547b7af09cd1c1314
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-22 22:06:46 +0545

    modified

--- first_game1/bullet.py
+++ first_game1/bullet.py
@@ -3,7 +3,7 @@ from pygame.sprite import Sprite
 
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
-
+    
     def __init__(self, ai_settings, screen, ship):
         """Create a bullet object at the ship's current position."""
         super(Bullet, self).__init__()
@@ -16,7 +16,7 @@ class Bullet(Sprite):
         
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
-        
+
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
@@ -25,7 +25,7 @@ class Bullet(Sprite):
             
             # Update the decimal position of the bullet.
             self.y -= self.speed_factor
-
+            
             # Update the rect position.
             self.rect.y = self.y
             
