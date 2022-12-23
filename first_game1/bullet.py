13263e32a33d68c2c5da7b7108bf3826a7b0d8c8
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-23 17:34:09 +0545

    modified

--- first_game1/bullet.py
+++ first_game1/bullet.py
@@ -22,10 +22,9 @@ class Bullet(Sprite):
 
         def update(self):
             """Move the bullet up the screen."""
-            
             # Update the decimal position of the bullet.
             self.y -= self.speed_factor
-            
+
             # Update the rect position.
             self.rect.y = self.y
             
