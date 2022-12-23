b60711f6893cb12d99200a09d71bd954c54ec19d
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-23 17:33:47 +0545

    modified

--- first_game1/alien_invasion.py
+++ first_game1/alien_invasion.py
@@ -1,9 +1,9 @@
 import sys
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from pygame.sprite import Group
 
 import game_functions as gf
 
@@ -15,7 +15,7 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    
     # Set the background color.
     bg_color = (0, 0, 230)
 
@@ -30,6 +30,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
+        bullets.update()
         gf.update_screen(ai_settings, screen, ship, bullets)
         
 run_game()
