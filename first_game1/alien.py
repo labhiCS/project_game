5b41766be7a6e9f92d6d9f204540425b920a73ae
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-24 18:49:59 +0545

    modified

--- first_game1/alien.py
+++ first_game1/alien.py
@@ -20,6 +20,19 @@ class Alien(Sprite):
         # Store the alien's exact position.
         self.x = float(self.rect.x)
         
-        def blitme(self):
-            """Draw the alien at its current location."""
-            self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the alien at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alienns right or or left."""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
