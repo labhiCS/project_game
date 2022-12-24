51af9e1bf04a76f97a256598f2ad812d69f51823
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-24 18:50:54 +0545

    modified

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -107,3 +107,7 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number,row_number)
+
+def update_aliens(aliens):
+    """Update the postions of all aliens in the fleet."""
+    aliens.update()
