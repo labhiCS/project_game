188c1a00e182885ff4e5252e0c7c4d9bca2ebf60
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-25 22:45:06 +0545

    Modified

--- first_game1/alien_invasion.py
+++ first_game1/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from game_stats import GameStats
 #from alien import Alien
 
 import game_functions as gf
@@ -17,6 +18,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
+
     # Set the background color.
     bg_color = (0, 0, 230)
 
@@ -37,7 +41,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, ship, aliens)
+        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         #bullets.update()
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         
