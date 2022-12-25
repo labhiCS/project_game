db087f1aaf47f1ecdb6a133401ab14415a376f54
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-25 22:44:29 +0545

    Responding to alien-ship Collisions

---
+++ first_game1/game_stats.py
@@ -0,0 +1,10 @@
+class GameStats():
+    """Track statistics for Alien Invasion."""
+    
+    def __init__(self, ai_settings):
+        self.ai_settings = ai_settings
+        self.reset_stats()
+    
+    def reset_stats(self):
+        """Initialize statistics that can change during"""
+        self.ships_left = self.ai_settings.ship_limit
