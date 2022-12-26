61599e907932c18e2ca823e9eb8ab6e1cfd9a3ed
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-26 23:18:05 +0545

    Added

--- first_game1/game_stats.py
+++ first_game1/game_stats.py
@@ -4,6 +4,9 @@ class GameStats():
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
+
+        # Start Alien Invasion in an active state.
+        self.game_active = True
     
     def reset_stats(self):
         """Initialize statistics that can change during"""
