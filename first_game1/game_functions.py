9be327dfb66de12351d681debd8e3d2ab2b5faad
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-27 22:54:25 +0545

    adding code to ship_hit()

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -135,20 +135,24 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to ship being hit by alien."""
-    # Decrement ships_left.
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # Decrement ships_left.
+        stats.ships_left -= 1
+        
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
     
-    # Empty the list of aliens and bullets.
-    aliens.empty()
-    bullets.empty()
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
     
-    # Create a new fleet and center the ship.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-    
-    # Pause.
-    sleep(0.5)
+        # Pause.
+        sleep(0.5)
 
+    else:
+        stats.game_active = False
+        
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if any aliens have reached the bottom of the screen."""
     screen_rect = screen.get_rect()
