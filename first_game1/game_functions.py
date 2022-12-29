21c50f22369bec83a125f2de92b630595808836c
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-29 20:41:57 +0545

    Starting game

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -35,7 +35,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,6 +53,14 @@ def check_events(ai_settings, screen, ship, bullets):
                 ship.moving_right = False
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                check_play_button(stats, play_button, mouse_x, mouse_y)
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Start a new game when the player clicks Play."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
