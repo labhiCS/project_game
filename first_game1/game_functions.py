666042bde6310d4472e7e3c90048d5bba7be4539
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-29 20:38:48 +0545

    Adding Play button

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -54,7 +54,7 @@ def check_events(ai_settings, screen, ship, bullets):
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -65,6 +65,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the play button if the game is inactive.
+    if not stats.game_active:
+        play_button.draw_button()
     
     # Make the most recently drawn screen visible.
     pygame.display.flip()
