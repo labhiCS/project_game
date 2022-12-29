03fc76ac263ee09596ebc40f0a623d7b506f9c93
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-29 20:47:00 +0545

    Resetting the Game

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -57,10 +57,20 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 check_play_button(stats, play_button, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game statistics.
+        stats.reset_stats()
         stats.game_active = True
+
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+        
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
