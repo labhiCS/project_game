936679269ed0f235ff7dfd15094ed57780680e1c
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-29 20:50:25 +0545

    Resetting the Game

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -35,7 +35,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -55,7 +55,7 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
                 ship.moving_left = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                check_play_button(stats, play_button, mouse_x, mouse_y)
+                check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play."""
