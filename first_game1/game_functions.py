3592ed9d67a45dcca44f7be7f3d0806861af2f92
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-30 21:56:21 +0545

    Deactivating the play button

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -62,6 +62,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     #if play_button.rect.collidepoint(mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
     
         # Reset the game statistics.
         stats.reset_stats()
@@ -177,6 +179,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
         
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if any aliens have reached the bottom of the screen."""
