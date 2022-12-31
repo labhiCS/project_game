b6a63265bc25ed98dd05965889924ca0d7789b0e
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-31 21:42:57 +0545

    Modifying the speed Settings

--- first_game1/settings.py
+++ first_game1/settings.py
@@ -1,7 +1,7 @@
 class Settings():
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's. static settings."""
         
         #Screen settings
         self.screen_width = 1200
@@ -22,6 +22,7 @@ class Settings():
         # Alien settings
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
 
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
