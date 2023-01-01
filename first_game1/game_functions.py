e44a8bb4836be9df1b44fa64e4b0d044324f2c5b
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-01 17:12:32 +0545

    Modified

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -79,7 +79,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -90,6 +90,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
     
     ship.blitme()
     aliens.draw(screen)
+
+    #Drawing the score information
+    sb.show_score()
 
     # Draw the play button if the game is inactive.
     if not stats.game_active:
