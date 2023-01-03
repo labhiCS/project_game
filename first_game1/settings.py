a851fe58f4c386d247e439b5de538364ca37db5f
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-03 22:11:23 +0545

    Modified

--- first_game1/settings.py
+++ first_game1/settings.py
@@ -25,22 +25,15 @@ class Settings():
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
     
-    def initialize_dynamic_settings(self):
-        """Initialize settings that change throughout the game."""
-        self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 3
-        self.alien_speed_factor = 1
-        
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
-
-        # Scoring
-        self.alien_points = 50
-    
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and and alienn point valuess."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
