5a7fe7087e7b1521aed6a4ae3174ffc83bd3abd7
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-03 22:01:37 +0545

    Upddating Method

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -109,11 +109,15 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
             
     check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Respond to bullet-alien collisions."""
     # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     
+    if collisions:
+        stats.score += ai_settings.alien_points
+        sb.prep_score()
+
     if len(aliens) == 0:
         # Destroy existing bullets, (speed up game) and create new fleet.
         bullets.empty()
