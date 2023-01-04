3e546de4552025cd07d0c0d68c2f4c6e91d77b41
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-04 23:06:17 +0545

    Store high score

--- first_game1/game_stats.py
+++ first_game1/game_stats.py
@@ -8,6 +8,9 @@ class GameStats():
         # Start Alien Invasion in an active state. = True
         # Start game in an inactive state.
         self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialize statistics that can change during"""
