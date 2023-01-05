224f21ae6cbe849290379f7434f132867063eae4
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-05 21:24:23 +0545

    Displaying the level

--- first_game1/game_stats.py
+++ first_game1/game_stats.py
@@ -15,3 +15,4 @@ class GameStats():
         """Initialize statistics that can change during"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
