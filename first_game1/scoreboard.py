a3012d59f0dc3e4e281eef36d1889f4df3649ee1
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-05 21:29:37 +0545

    Showing ship

--- first_game1/scoreboard.py
+++ first_game1/scoreboard.py
@@ -53,6 +53,7 @@ class Scoreboard():
         self.high_score_rect.top = self.score_rect.top
     
     def show_score(self):
-        """Draw the score to the screen."""
+        """Draw the score and ship to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)#Showing ship
