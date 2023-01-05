720edfeefcd7c208e410c002e2e391fd01610a6c
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-05 21:23:23 +0545

    Call check_high_score().

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -118,7 +118,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
-            check_high_score(stats, sb)
+        check_high_score(stats, sb)
 
     if len(aliens) == 0:
         # Destroy existing bullets, (speed up game) and create new fleet.
