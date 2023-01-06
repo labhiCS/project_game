670b8fa88e1a07d441936e345aaecdfbc819e3ec
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-06 21:49:24 +0545

    calling prep_ships

--- first_game1/scoreboard.py
+++ first_game1/scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard():
     """A class to report scoring information."""
@@ -18,6 +21,16 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
     def prep_level(self):
         """Turn the level into a rendered image."""
