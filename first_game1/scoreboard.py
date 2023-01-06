a8e30d8afb217030f3d484b7161ef2b906bac568
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-06 22:02:35 +0545

    Modified

--- first_game1/scoreboard.py
+++ first_game1/scoreboard.py
@@ -70,3 +70,6 @@ class Scoreboard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)#Showing ship
+
+        #Draw ships!!
+        self.ships.draw(self.screen)
