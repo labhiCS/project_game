652ebcb7032b721c2d8542614d744820f9b8c88f
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-06 21:58:37 +0545

    Adding parameter

--- first_game1/game_functions.py
+++ first_game1/game_functions.py
@@ -206,13 +206,13 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
         
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Check if any aliens have reached the bottom of the screen."""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # Treat this the same as if the ship got hit.
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
             break
 
 def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
