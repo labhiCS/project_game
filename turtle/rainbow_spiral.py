b22d562a2ccdb015fc658f52f935868c9845ace9
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-11 00:21:43 +0545

    "Rainbow Spiral."

---
+++ turtle/rainbow_spiral.py
@@ -0,0 +1,29 @@
+# draw rainbouw spiral
+# turtle graphic
+
+import turtle
+from turtle import *
+
+turtle.title("rainbow spiral")
+speed(15)
+bgcolor("black")
+r, g , b = 255, 0, 0
+
+for i in range(255 * 2):
+    colormode(255)
+    if i < 255 // 3:
+        g += 3
+    elif i < 255*2 //3:
+        r-=3
+    elif i<255:
+        b += 3
+    elif i < 255*4 //3:
+        g -= 3
+    elif i < 255*5 // 3:
+        r += 3
+    else:
+        b -= 3
+    fd(50 + 1)
+    rt(91)
+    pencolor(r, g, b)
+    
