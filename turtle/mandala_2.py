55a3a81c7c3b4da1461a036d4b7295ad4329765e
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-13 23:13:03 +0545

    "Amazing Design_2"

---
+++ turtle/mandala_2.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+l = ["red", "yellow", "pink", "purple", "white"]
+turtle.bgcolor("black")
+
+for i in range(200):
+    t.speed(0)
+    t.color(l[i%5])
+    t.pensize(i/50+1)
+    t.forward(i)
+    t.left(59)
+
+turtle.mainloop()
