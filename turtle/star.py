ea1000a95ab715d40c49a9f7f97de40ecba0de5f
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-13 22:53:02 +0545

    "Making star using Python turtle"

--- turtle/star.py
+++ turtle/star.py
@@ -3,8 +3,10 @@ import turtle
 turtle.color("yellow")
 turtle.Screen().bgcolor("black")
 
-turtle.width(12)
+turtle.width(16)
 
 for i in range(5):
-    turtle.forward(150)
+    turtle.forward(190)
     turtle.right(144)
+
+turtle.done()
