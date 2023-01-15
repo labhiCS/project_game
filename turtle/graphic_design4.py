3bbe8dd22b3d9e78e25a08fd9fa43ac0ee0facf7
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-15 09:14:26 +0545

    "Graphic Design4"

---
+++ turtle/graphic_design4.py
@@ -0,0 +1,18 @@
+from turtle import *
+from colorsys import *
+pensize(4)
+h = 0.4
+for i in range(180, 30, -30):
+    c = hsv_to_rgb(h, 1, 1)
+    color("black")
+    fillcolor(c)
+
+    begin_fill()
+    for j in range(6):
+        for k in range(6):
+            h += 0.005
+            fd(i)
+            lt(60)
+        lt(60)
+    end_fill()
+done()
