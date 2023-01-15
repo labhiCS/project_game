bf056301e45ea7c08873c0a698a2aa211434a83d
labhiCS <lamichhaneabhi10@gmail.com>
2023-01-15 09:07:52 +0545

    "Graphic design3"

---
+++ turtle/graphic_design3.py
@@ -0,0 +1,15 @@
+from turtle import *
+from colorsys import*
+bgcolor("black")
+speed(0)
+pensize(4)
+h = 0
+for i in range(550):
+    c = hsv_to_rgb(h, 1, 1)
+    color(c)
+    h += 0.005
+    circle(200-i, 30)
+    lt(220)
+    circle(200-i, 30)
+    lt(40)
+done()
