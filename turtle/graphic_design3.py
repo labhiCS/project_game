5c6e510485d9683e03806d46c69405ad94b07575
labhiCS <lamichhane10@gmail.com>
2023-01-16 22:41:59 +0545

    "graphic Design3"

--- turtle/graphic_design3.py
+++ turtle/graphic_design3.py
@@ -8,8 +8,8 @@ for i in range(550):
     c = hsv_to_rgb(h, 1, 1)
     color(c)
     h += 0.005
-    circle(200-i, 30)
+    circle(200-i, 25)
     lt(220)
-    circle(200-i, 30)
+    circle(200-i, 25)
     lt(40)
 done()
